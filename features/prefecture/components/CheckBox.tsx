7bab86aebbd2019c5580f2cb14e9be18e734c50a
GigaOwO <takumagiga1118@gmail.com>
2025-10-10 10:30:11 +0900

    ref: features/prefectureのファイルにtsdocを追加

--- features/prefecture/components/CheckBox.tsx
+++ features/prefecture/components/CheckBox.tsx
@@ -1,9 +1,30 @@
+/**
+ * CheckBoxコンポーネントのプロパティ
+ */
 type Props = {
+  /** 都道府県名 */
   prefName: string;
+  /** 都道府県コード */
   prefCode: number;
+  /** チェックボックスの状態が変更されたときのコールバック関数 */
   onToggle: (prefCode: number, prefName: string, isChecked: boolean) => void;
 };
 
+/**
+ * 都道府県選択用のチェックボックスコンポーネント
+ *
+ * @param props - コンポーネントのプロパティ
+ * @returns チェックボックス要素
+ *
+ * @example
+ * ```tsx
+ * <CheckBox
+ *   prefName="東京都"
+ *   prefCode={13}
+ *   onToggle={(code, name, checked) => console.log(code, name, checked)}
+ * />
+ * ```
+ */
 export default function CheckBox({ prefName, prefCode, onToggle }: Props) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     onToggle(prefCode, prefName, e.target.checked);
